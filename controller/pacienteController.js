3ab7d917dd4d690d8e1e261e54dabded31f9c27d
LEONARDO DE NOVAES VIVI <21276367@dsn127m1246733.cfp127aluno.local>
2022-10-27 16:47:17 -0300

    fazendo verificacao para saber se o telefone é valido

--- controller/pacienteController.js
+++ controller/pacienteController.js
@@ -18,30 +18,30 @@ const listarPaciente = async (req,res) => {
 const cadastrarPaciente = async (req,res) => {
     let { nome_paciente, telefone_paciente, celular_paciente, email_paciente, nome_responsavel, telefone_responsavel } = req.body;
 
-   if (nome_paciente != null && telefone_paciente != null && celular_paciente != null && email_paciente != null) {
-
-        pacienteModel.create({
-            nome_paciente,
-            telefone_paciente,
-            celular_paciente,
-            email_paciente,
-            nome_responsavel,
-            telefone_responsavel
-            })
-            .then(
-                () => {
-                res.status(200).json({message:"inserido com sucesso."});
-                }
-            )
-            .catch(
-                (error) => {
-                res.status(500).json({ message: error });
-                }
-            );
-   } else {
-      res.status(500).json({ msg: "Existem campos obrigatórios que devem ser preenchidos" })
-   }
-
+    if (nome_paciente != null && telefone_paciente != null && celular_paciente != null && email_paciente != null) {
+        if(Number(telefone_paciente) && Number(celular_paciente)){
+            pacienteModel.create({
+                nome_paciente,
+                telefone_paciente,
+                celular_paciente,
+                email_paciente,
+                nome_responsavel,
+                telefone_responsavel
+                })
+                .then(
+                    () => {
+                    res.status(200).json({message:"inserido com sucesso."});
+                    }
+                )
+                .catch(
+                    (error) => {
+                    res.status(500).json({ message: error });
+                    }
+                );
+        } else {
+        res.status(500).json({ msg: "Existem campos obrigatórios que devem ser preenchidos" })
+        }
+    }
 }
 
 module.exports = {
